2ec1322cbecced477826652c567ed8bc2d31c952
Phil Wang <lucidrains@gmail.com>
2021-02-10 17:46:33 -0800

    add ability to attend to k-nearest neighbors, to cutdown on attention between faraway points

--- README.md
+++ README.md
@@ -22,10 +22,31 @@ attn = PointTransformerLayer(
     attn_mlp_hidden_mult = 4
 )
 
-x = torch.randn(1, 16, 128)
+feats = torch.randn(1, 16, 128)
 pos = torch.randn(1, 16, 3)
+mask = torch.ones(1, 16).bool()
 
-attn(x, pos) # (1, 16, 128)
+attn(feats, pos, mask = mask) # (1, 16, 128)
+```
+
+This type of vector attention is much more expensive than the traditional one. In the paper, they used k-nearest neighbors on the points to exclude attention on faraway points. You can do the same with a single extra setting.
+
+```python
+import torch
+from point_transformer_pytorch import PointTransformerLayer
+
+attn = PointTransformerLayer(
+    dim = 128,
+    pos_mlp_hidden_dim = 64,
+    attn_mlp_hidden_mult = 4,
+    num_neighbors = 16          # only the 16 nearest neighbors would be attended to for each point
+)
+
+feats = torch.randn(1, 2048, 128)
+pos = torch.randn(1, 2048, 3)
+mask = torch.ones(1, 2048).bool()
+
+attn(feats, pos, mask = mask) # (1, 16, 128)
 ```
 
 ## Citations
--- point_transformer_pytorch/point_transformer_pytorch.py
+++ point_transformer_pytorch/point_transformer_pytorch.py
@@ -1,5 +1,30 @@
+import torch
 from torch import nn, einsum
 from einops import repeat
+
+# helpers
+
+def exists(val):
+    return val is not None
+
+def max_value(t):
+    return torch.finfo(t.dtype).max
+
+def batched_index_select(values, indices, dim = 1):
+    value_dims = values.shape[(dim + 1):]
+    values_shape, indices_shape = map(lambda t: list(t.shape), (values, indices))
+    indices = indices[(..., *((None,) * len(value_dims)))]
+    indices = indices.expand(*((-1,) * len(indices_shape)), *value_dims)
+    value_expand_len = len(indices_shape) - (dim + 1)
+    values = values[(*((slice(None),) * dim), *((None,) * value_expand_len), ...)]
+
+    value_expand_shape = [-1] * len(values.shape)
+    expand_slice = slice(dim, (dim + value_expand_len))
+    value_expand_shape[expand_slice] = indices.shape[expand_slice]
+    values = values.expand(*value_expand_shape)
+
+    dim += value_expand_len
+    return values.gather(dim, indices)
 
 # classes
 
@@ -9,9 +34,12 @@ class PointTransformerLayer(nn.Module):
         *,
         dim,
         pos_mlp_hidden_dim = 64,
-        attn_mlp_hidden_mult = 4
+        attn_mlp_hidden_mult = 4,
+        num_neighbors = None
     ):
         super().__init__()
+        self.num_neighbors = num_neighbors
+
         self.to_qkv = nn.Linear(dim, dim * 3, bias = False)
 
         self.pos_mlp = nn.Sequential(
@@ -26,25 +54,51 @@ class PointTransformerLayer(nn.Module):
             nn.Linear(dim * attn_mlp_hidden_mult, dim),
         )
 
-    def forward(self, x, pos):
-        n = x.shape[1]
+    def forward(self, x, pos, mask = None):
+        n, num_neighbors = x.shape[1], self.num_neighbors
 
         # get queries, keys, values
         q, k, v = self.to_qkv(x).chunk(3, dim = -1)
 
         # calculate relative positional embeddings
-        rel_pos = pos[:, :, None] - pos[:, None, :]
+        rel_pos = pos[:, :, None, :] - pos[:, None, :, :]
         rel_pos_emb = self.pos_mlp(rel_pos)
 
         # use subtraction of queries to keys. i suppose this is a better inductive bias for point clouds than dot product
-        qk_rel = q[:, :, None] - k[:, None, :]
+        qk_rel = q[:, :, None, :] - k[:, None, :, :]
 
-        # use attention mlp, making sure to add relative positional embedding first
-        sim = self.attn_mlp(qk_rel + rel_pos_emb).squeeze(dim = -1)
+        # prepare mask
+        if exists(mask):
+            mask = mask[:, :, None] * mask[:, None, :]
+
+        # expand values
+        v = repeat(v, 'b j d -> b i j d', i = n)
+
+        # determine k nearest neighbors for each point, if specified
+        if exists(num_neighbors) and num_neighbors < n:
+            rel_dist = rel_pos.norm(dim = -1)
+
+            if exists(mask):
+                mask_value = max_value(rel_dist)
+                rel_dist.masked_fill_(~mask, mask_value)
+
+            dist, indices = rel_dist.topk(num_neighbors, largest = False)
+
+            v = batched_index_select(v, indices, dim = 2)
+            qk_rel = batched_index_select(qk_rel, indices, dim = 2)
+            rel_pos_emb = batched_index_select(rel_pos_emb, indices, dim = 2)
+            mask = batched_index_select(mask, indices, dim = 2) if exists(mask) else None
 
         # expand transformed features and add relative positional embeddings
-        v = repeat(v, 'b j d -> b i j d', i = n)
         v = v + rel_pos_emb
+
+        # use attention mlp, making sure to add relative positional embedding first
+        sim = self.attn_mlp(qk_rel + rel_pos_emb)
+
+        # masking
+        if exists(mask):
+            mask_value = -max_value(sim)
+            sim.masked_fill_(~mask[..., None], mask_value)
 
         # attention
         attn = sim.softmax(dim = -2)
